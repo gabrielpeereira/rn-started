08df6681937457c3470c0929e6aa3da8c2169307
Gabriel Pereira <gabriel.developer@outlook.com>
2020-07-02 18:01:17 -0300

    adcionando cores com botões de incremento

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 const SquareScreen = () => {
+
+    const COLOR_INCREMENT = 15;
+
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
+
     return(
         <View>
-            <ColorCounter color="Red" />
-            <ColorCounter color="Blue"/>
-            <ColorCounter color="Green"/>
+            <ColorCounter
+                onIncrease={() => setRed(red + COLOR_INCREMENT) }
+                onDecrease={() => setRed(red - COLOR_INCREMENT) }
+                color="Red" 
+            />
+            <ColorCounter 
+                onIncrease={() => setGreen(green + COLOR_INCREMENT) }
+                onDecrease={() => setGreen(green - COLOR_INCREMENT) }
+                color="Green"
+            />
+            <ColorCounter
+                onIncrease={() => setBlue(blue + COLOR_INCREMENT) }
+                onDecrease={() => setBlue(blue - COLOR_INCREMENT) }
+                color="Blue"
+            />
+            <View style={{ height: 100, width: 150, backgroundColor: `rgb(${red},${green},${blue})`}}></View>
         </View>
     )
 }
