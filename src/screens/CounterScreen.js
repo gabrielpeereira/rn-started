b1400ec61b36560c9c9f9d7131b599619d841bbd
Gabriel Pereira <gabriel.developer@outlook.com>
2020-07-06 21:04:48 -0300

    adicionando exercicios de react native

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,20 +2,25 @@ import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const reducer = (state, action) => {
-
+    switch(action.type){
+        case 'change_increase':
+            return { ...state, count: state.count + 1 }
+        case 'change_decrease':
+            return { ...state, count: state.count - 1 }
+        default:
+    }
 }
 
 const CouterScreen = () => {
-    const [state, dispatch] = useReducer(reducer, 0);
+    const [state, dispatch] = useReducer(reducer, {count: 0});
     return(
         <View>
-            <Button title="Increase" onPress={() => {
-               setCounter(counter + 1)
-            }} />
-            <Button title="Decrease" onPress={() => {
-                setCounter(counter - 1)
-            }}/>
-            <Text>Current Count: {counter}</Text>
+            <Button title="change_increase" 
+              onPress={() => dispatch({ type: 'change_increase' })}
+             />
+            <Button title="Decrease" 
+            onPress={() => dispatch({ type: 'change_decrease' })}/>
+            <Text>Current Count: {state.count}</Text>
         </View>
     )
 };
